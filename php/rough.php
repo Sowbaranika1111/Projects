2f3bad7be19af12d9af08ac36d2efff5975ef314
Sowbaranika1111 <91730517+Sowbaranika1111@users.noreply.github.com>
2024-03-10 14:05:36 +0530

    Update rough.php

--- php/rough.php
+++ php/rough.php
@@ -1,100 +1,105 @@
 <?php
-// class Product{
-//     public $name = 'soap';
-//     public $price; //in dollars
-//     public $rate = 'Rs.20';
+class Product{
+    public $name = 'soap';
+    public $price; //in dollars
+    public $rate = 'Rs.20';
 
-//     public function priceInIndianRs()
-//     {
-//         $Indianprice = $this -> price * 82;
-//         return $Indianprice ;
-//     }
-// }
-// $soapObject1 = new Product();
-// $soapObject2 = new Product();
+    public function priceInIndianRs()
+    {
+        $Indianprice = $this -> price * 82;
+        return $Indianprice ;
+    }
+}
+$soapObject1 = new Product();
+$soapObject2 = new Product();
 
-// $combObject = new Product();
+$combObject = new Product();
 
-// print $soapObject -> name."<br>";
-// $soapObject -> name = 'toothpaste';
-// print $soapObject -> name."<br>";
+print $soapObject -> name."<br>";
+$soapObject -> name = 'toothpaste';
+print $soapObject -> name."<br>";
 
-// $soapObject1 -> price = "10";
-// $soapObject2 -> price = "20";
-// $Indianprice = $soapObject2 -> priceInIndianRs();
-// echo $Indianprice."<br>";
-
-
-// $Indianprice  = $soapObject1 -> priceInIndianRs();
-// echo $Indianprice ;
-
-// print $combObject -> rate."<br>";
-// $combObject -> rate = "Rs.25";
-// print $combObject->rate;
+$soapObject1 -> price = "10";
+$soapObject2 -> price = "20";
+$Indianprice = $soapObject2 -> priceInIndianRs();
+echo $Indianprice."<br>";
 
 
-// class Fruit {
-//   // Properties
-//   public $name;
-//   public $color;
+$Indianprice  = $soapObject1 -> priceInIndianRs();
+echo $Indianprice ;
 
-//   //methods
-//   function set_name($name) {
-//     $this->name = $name;
-//   }
-//   function get_name() {
-//     return $this->name;
-//   }
-// }
+print $combObject -> rate."<br>";
+$combObject -> rate = "Rs.25";
+print $combObject->rate;
 
-// $apple = new Fruit();
-// $banana = new Fruit();
-// $apple->set_name('Apple');
-// $banana->set_name('Banana');
+//============================================================================================================================================
 
-// echo $apple->get_name();
-// echo "<br>";
-// echo $banana->get_name();
+class Fruit {
+  // Properties
+  public $name;
+  public $color;
 
+  //methods
+  function set_name($name) {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+}
 
-// class Flowers
-// {
-//   //properties
-//   public $flowerName;
-//   public $flowerColor;
+$apple = new Fruit();
+$banana = new Fruit();
+$apple->set_name('Apple');
+$banana->set_name('Banana');
 
-//   //methods
-//   function set_name_color($flowerName,$flowerColor){
-//     $this->flowerName  = $flowerName;
-//     $this->flowerColor = $flowerColor;
-//   }
-//   //1st method storing in array
-//   function getNameColor_mthd_1(){
-//     return array($this->flowerName,$this->flowerColor);
-//   }
-//   //2nd method storing in associative array
-//   function get_name_color_mthd_2(){
-//     return array('flowerName' => $this -> flowerName ,
-//                  'flowerColor'=> $this -> flowerColor) ;
-//   }
-// }
-// $flowerObj1 = new Flowers();
-// $flowerObj2 = new Flowers();
+echo $apple->get_name();
+echo "<br>";
+echo $banana->get_name();
 
-// $flowerObj1 -> set_name_color("Lily","Pink");
-// $flowerObj2 -> set_name_color("Jasmine","White");
+//============================================================================================================================================
 
-// $result1 = $flowerObj1 -> getNameColor_mthd_1();
-// $result2 = $flowerObj2 -> get_name_color_mthd_2();
+class Flowers
+{
+  //properties
+  public $flowerName;
+  public $flowerColor;
 
-// //getting name,color via first method
-// echo $flowerName = $result1[0]."<br>";
+  //methods
+  function set_name_color($flowerName,$flowerColor){
+    $this->flowerName  = $flowerName;
+    $this->flowerColor = $flowerColor;
+  }
+  //1st method storing in array
+  function getNameColor_mthd_1(){
+    return array($this->flowerName,$this->flowerColor);
+  }
+  //2nd method storing in associative array
+  function get_name_color_mthd_2(){
+    return array('flowerName' => $this -> flowerName ,
+                 'flowerColor'=> $this -> flowerColor) ;
+  }
+}
+$flowerObj1 = new Flowers();
+$flowerObj2 = new Flowers();
 
-// //getting name,color via second method
-// echo $flowerColor = $result2['flowerColor'];
+$flowerObj1 -> set_name_color("Lily","Pink");
+$flowerObj2 -> set_name_color("Jasmine","White");
 
-//using a constructor and providing the necessary parameters while creating the object itself
-//thus eliminating the need for creating a separate method for parameters assignment
+$result1 = $flowerObj1 -> getNameColor_mthd_1();
+$result2 = $flowerObj2 -> get_name_color_mthd_2();
+
+//getting name,color via first method
+echo $flowerName = $result1[0]."<br>";
+
+//getting name,color via second method
+echo $flowerColor = $result2['flowerColor'];
+
+using a constructor and providing the necessary parameters while creating the object itself
+thus eliminating the need for creating a separate method for parameters assignment
+
+//============================================================================================================================================
+  
 class FlowerShop
 {
   public $name;
