50bb46558a46e633930908fb42436c4ff6966b2f
Sowbaranika-G-002 <91730517+Sowbaranika-G-002@users.noreply.github.com>
2024-03-15 20:32:26 +0530

    Deleted existing files

--- travelWebsiteHomePg/js/logout.js
+++
@@ -1,6 +0,0 @@
-let logoutButton = document.getElementById("logout")
-logoutButton.addEventListener('click', function (event) {
-    event.preventDefault()
-    location.replace("loginPg.html")
-})
-
